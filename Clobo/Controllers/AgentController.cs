b9c934adbf5d5e4265aabd3e57be38743aa02af2
cliffordbateman <cliffbatemandev@gmail.com>
2022-11-08 21:11:08 -0500

    scaffolding for agents

---
+++ Clobo/Controllers/AgentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Clobo.Controllers
+{
+    public class AgentController : ApiControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public IActionResult Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
