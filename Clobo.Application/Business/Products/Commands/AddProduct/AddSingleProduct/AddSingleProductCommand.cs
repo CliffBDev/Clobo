0995b4db1034b9317f5220b5e7d32c7781612a8a
cliffordbateman <cliffbatemandev@gmail.com>
2022-12-07 01:54:46 -0500

    getting commands ready. rough outlining the structure of this workflow.

---
+++ Clobo.Application/Business/Products/Commands/AddProduct/AddSingleProduct/AddSingleProductCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Clobo.Application.Common.Interfaces;
+using Clobo.Domain.Entities;
+using MediatR;
+
+namespace Clobo.Application.Business.Products.Commands.AddProduct.AddSingleProduct
+{
+    public record AddSingleProductCommand : IRequest<Product>
+    {
+        public string Name { get; init; }
+        public string? SerialNumber { get; init; }
+        public string Description { get; init; }
+        public double Price { get; init; }
+    }
+
+    public class AddSingleProductCommandHandler : IRequestHandler<AddSingleProductCommand, Product>
+    {
+        IApplicationDbContext _context;
+
+        public AddSingleProductCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Product> Handle(AddSingleProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
