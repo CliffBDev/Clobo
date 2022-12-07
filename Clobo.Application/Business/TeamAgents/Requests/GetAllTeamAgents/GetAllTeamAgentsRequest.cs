7b900f1613c710b91df928e0d4f084059a33baae
cliffordbateman <cliffbatemandev@gmail.com>
2022-12-07 01:14:38 -0500

    changes on the team controller. couldn’t get the project to pick up changes. think it ended up being an issue with vs for mac.

---
+++ Clobo.Application/Business/TeamAgents/Requests/GetAllTeamAgents/GetAllTeamAgentsRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Clobo.Application.Common.Interfaces;
+using Clobo.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clobo.Application.Business.TeamAgents.Requests.GetAllTeamAgents
+{
+    public record GetAllTeamAgentsRequest : IRequest<IList<TeamAgent>>;
+
+    public class GetAllTeamAgentsRequestHandler : IRequestHandler<GetAllTeamAgentsRequest, IList<TeamAgent>>
+    {
+        IApplicationDbContext _context;
+        public GetAllTeamAgentsRequestHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IList<TeamAgent>> Handle(GetAllTeamAgentsRequest request, CancellationToken cancellationToken)
+        {
+            return await _context.TeamAgents.Include(x => x.Agent).Include(x => x.Team).ToListAsync();
+        }
+    }
+}
+
