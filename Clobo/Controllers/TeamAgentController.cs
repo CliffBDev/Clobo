507c6a240075763247d49096bd49bcb7feb9ade2
cliffordbateman <cliffbatemandev@gmail.com>
2022-12-07 20:25:59 -0500

    Fixed the weird issues with the team agent controller.

--- Clobo/Controllers/TeamAgentController.cs
+++ Clobo/Controllers/TeamAgentController.cs
@@ -14,17 +14,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Clobo.Controllers
 {
-    [Route("api/teamAgent")]
-    [ApiController]
-    public class TeamAgentController : ControllerBase
+    public class TeamAgentController : ApiControllerBase
     {
-        private ISender _mediator;
-
-        public TeamAgentController(ISender mediator)
-        {
-            _mediator = mediator;
-        }
-
         //I don't really like the verby names for endpoints but here we are.
         //This is the only thing I could think of to achieve the workflow I am aiming for.
 
@@ -32,7 +23,7 @@ namespace Clobo.Controllers
         [ProducesResponseType(typeof(IList<TeamAgent>), StatusCodes.Status200OK)]
         public async Task<IActionResult> Get()
         {
-            var res = await _mediator.Send(new GetAllTeamAgentsRequest());
+            var res = await Mediator.Send(new GetAllTeamAgentsRequest());
             return Ok(res);
         }
 
@@ -40,7 +31,7 @@ namespace Clobo.Controllers
         [ProducesResponseType(typeof(TeamAgent), StatusCodes.Status200OK)]
         public async Task<IActionResult> AddAgentToTeam([FromBody] AddAgentToTeamCommand command)
         {
-            var res = await _mediator.Send(command);
+            var res = await Mediator.Send(command);
             return Ok(res);
         }
 
@@ -48,7 +39,7 @@ namespace Clobo.Controllers
         [ProducesResponseType(typeof(TeamAgent), StatusCodes.Status200OK)]
         public async Task<IActionResult> MakeAgentTeamLead([FromBody] AgentTeamLeadCommand command)
         {
-            var res = await _mediator.Send(command);
+            var res = await Mediator.Send(command);
             return Ok(res);
         }
 
@@ -56,7 +47,7 @@ namespace Clobo.Controllers
         [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
         public async Task<IActionResult> MakeAgentTeamLead([FromBody] RemoveAgentFromTeamCommand command)
         {
-            var res = await _mediator.Send(command);
+            var res = await Mediator.Send(command);
             return Ok(res);
         }
     }
