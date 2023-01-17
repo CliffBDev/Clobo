9c38a9f27b6a28d72b67458a25232387d0b83621
cliffordbateman <cliffbatemandev@gmail.com>
2023-01-17 01:18:20 -0500

    Note ticket commands, requests and controller.
    Note ticket commands, requests and controller.

---
+++ Clobo.Application/Business/TicketNotes/Commands/AddTicketNote/AddTicketNoteCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentValidation;
+
+namespace Clobo.Application.Business.TicketNotes.Commands.AddTicketNote
+{
+    public class AddTicketNoteCommandValidator : AbstractValidator<AddTicketNoteCommand>
+    {
+        public AddTicketNoteCommandValidator()
+        {
+            RuleFor(x => x.TicketId)
+                .GreaterThan(0).WithMessage("Ticket Id must be greater than 0");
+
+            RuleFor(x => x.AgentId)
+                .GreaterThan(0).WithMessage("Agent Id must be greater than 0");
+
+            RuleFor(x => x.Note)
+                .NotEmpty().WithMessage("Note cannot be empty");
+        }
+    }
+}
+
---
+++ Clobo.Application/Business/TicketNotes/Requests/GetTicketNotes/GetTicketNotesRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Clobo.Application.Common.Interfaces;
+using Clobo.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clobo.Application.Business.TicketNotes.Requests.GetTicketNotes
+{
+    public record GetTicketNotesRequest : IRequest<IList<TicketNote>>
+    {
+        public int TicketId { get; init; }
+    }
+
+    public class GetTicketNotesRequestHandler : IRequestHandler<GetTicketNotesRequest, IList<TicketNote>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetTicketNotesRequestHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IList<TicketNote>> Handle(GetTicketNotesRequest request, CancellationToken cancellationToken)
+        {
+            var ticket = await _context.Tickets.Include(x => x.TicketNotes).ThenInclude(x => x.Agent).FirstOrDefaultAsync(x => x.Id == request.TicketId);
+
+            if (ticket is null)
+                throw new ArgumentNullException("Ticket could not be found");
+
+            return ticket.TicketNotes;
+        }
+    }
+}
+
